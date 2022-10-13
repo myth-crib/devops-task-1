03aad54203d934ca4d09d13348bdef3d06b080e0
EC2 Default User <ec2-user@ip-172-31-36-78.ap-southeast-1.compute.internal>
2022-10-13 10:55:27 +0000

    IAC with terraform provider aws

---
+++ terraform.tf
@@ -0,0 +1,379 @@
+# Specify the provider and access details
+provider "aws" {
+  region = "us-east-1"
+  access_key = "*****"
+  secret_key = "*****"
+}
+
+# Creating VPC
+resource "aws_vpc" "demovpc" {
+  cidr_block       = "${var.vpc_cidr}"
+  instance_tenancy = "default"tags = {
+    Name = "Demo VPC"
+  }
+}
+# Creating 1st web subnet
+resource "aws_subnet" "public-subnet-1" {
+  vpc_id                  = "${aws_vpc.demovpc.id}"
+  cidr_block             = "${var.subnet_cidr}"
+  map_public_ip_on_launch = true
+  availability_zone = "us-east-1a"
+tags = {
+    Name = "Web Subnet 1"
+  }
+}
+# Creating 2nd web subnet
+resource "aws_subnet" "public-subnet-2" {
+  vpc_id                  = "${aws_vpc.demovpc.id}"
+  cidr_block             = "${var.subnet1_cidr}"
+  map_public_ip_on_launch = true
+  availability_zone = "us-east-1b"
+tags = {
+    Name = "Web Subnet 2"
+  }
+}
+# Creating 1st application subnet
+resource "aws_subnet" "application-subnet-1" {
+  vpc_id                  = "${aws_vpc.demovpc.id}"
+  cidr_block             = "${var.subnet2_cidr}"
+  map_public_ip_on_launch = false
+  availability_zone = "us-east-1a"
+tags = {
+    Name = "Application Subnet 1"
+  }
+}
+# Creating 2nd application subnet
+resource "aws_subnet" "application-subnet-2" {
+  vpc_id                  = "${aws_vpc.demovpc.id}"
+  cidr_block             = "${var.subnet3_cidr}"
+  map_public_ip_on_launch = false
+  availability_zone = "us-east-1b"
+tags = {
+    Name = "Application Subnet 2"
+  }
+}
+# Create Database Private Subnet
+resource "aws_subnet" "database-subnet-1" {
+  vpc_id            = "${aws_vpc.demovpc.id}"
+  cidr_block        = "${var.subnet4_cidr}"
+  availability_zone = "us-east-1a"
+tags = {
+    Name = "Database Subnet 1"
+  }
+}
+# Create Database Private Subnet
+resource "aws_subnet" "database-subnet-2" {
+  vpc_id            = "${aws_vpc.demovpc.id}"
+  cidr_block        = "${var.subnet5_cidr}"
+  availability_zone = "us-east-1a"
+tags = {
+    Name = "Database Subnet 1"
+  }
+}
+
+# Creating Internet Gateway
+resource "aws_internet_gateway" "demogateway" {
+  vpc_id = "${aws_vpc.demovpc.id}"
+}
+
+# Creating Route Table
+resource "aws_route_table" "route" {
+    vpc_id = "${aws_vpc.demovpc.id}"
+route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = "${aws_internet_gateway.demogateway.id}"
+    }
+tags = {
+        Name = "Route to internet"
+    }
+}
+# Associating Route Table
+resource "aws_route_table_association" "rt1" {
+    subnet_id = "${aws_subnet.demosubnet.id}"
+    route_table_id = "${aws_route_table.route.id}"
+}
+# Associating Route Table
+resource "aws_route_table_association" "rt2" {
+    subnet_id = "${aws_subnet.demosubnet1.id}"
+    route_table_id = "${aws_route_table.route.id}"
+}
+
+# Creating 1st EC2 instance in Public Subnet
+resource "aws_instance" "demoinstance" {
+  ami                         = "ami-087c17d1fe0178315"
+  instance_type               = "t2.micro"
+  count                       = 1
+  key_name                    = "tests"
+  vpc_security_group_ids      = ["${aws_security_group.demosg.id}"]
+  subnet_id                   = "${aws_subnet.demoinstance.id}"
+  associate_public_ip_address = true
+  user_data                   = "${file("data.sh")}"
+tags = {
+    Name = "My Public Instance"
+  }
+}
+# Creating 2nd EC2 instance in Public Subnet
+resource "aws_instance" "demoinstance1" {
+  ami                         = "ami-087c17d1fe0178315"
+  instance_type               = "t2.micro"
+  count                       = 1
+  key_name                    = "tests"
+  vpc_security_group_ids      = ["${aws_security_group.demosg.id}"]
+  subnet_id                   = "${aws_subnet.demoinstance.id}"
+  associate_public_ip_address = true
+  user_data                   = "${file("data.sh")}"
+tags = {
+    Name = "My Public Instance 2"
+  }
+}
+
+# Creating Security Group
+resource "aws_security_group" "demosg" {
+  vpc_id = "${aws_vpc.demovpc.id}"
+# Inbound Rules
+  # HTTP access from anywhere
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+# HTTPS access from anywhere
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+# SSH access from anywhere
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+# Outbound Rules
+  # Internet access to anywhere
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+tags = {
+    Name = "Web SG"
+  }
+}
+
+# Create Database Security Group
+resource "aws_security_group" "database-sg" {
+  name        = "Database SG"
+  description = "Allow inbound traffic from application layer"
+  vpc_id      = aws_vpc.demovpc.id
+ingress {
+    description     = "Allow traffic from application layer"
+    from_port       = 3306
+    to_port         = 3306
+    protocol        = "tcp"
+    security_groups = [aws_security_group.demosg.id]
+  }
+egress {
+    from_port   = 32768
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+tags = {
+    Name = "Database SG"
+  }
+
+# Creating External LoadBalancer
+resource "aws_lb" "external-alb" {
+  name               = "External LB"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.demosg.id]
+  subnets            = [aws_subnet.public-subnet-1.id, aws_subnet.public-subnet-1.id]
+}
+resource "aws_lb_target_group" "target-elb" {
+  name     = "ALB TG"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.demovpc.id
+}
+resource "aws_lb_target_group_attachment" "attachment" {
+  target_group_arn = aws_lb_target_group.external-alb.arn
+  target_id        = aws_instance.demoinstance.id
+  port             = 80
+depends_on = [
+    aws_instance.demoinstance,
+  ]
+}
+resource "aws_lb_target_group_attachment" "attachment" {
+  target_group_arn = aws_lb_target_group.external-alb.arn
+  target_id        = aws_instance.demoinstance1.id
+  port             = 80
+depends_on = [
+    aws_instance.demoinstance1,
+  ]
+}
+resource "aws_lb_listener" "external-elb" {
+  load_balancer_arn = aws_lb.external-alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.external-alb.arn
+  }
+}
+
+# Creating RDS Instance
+resource "aws_db_subnet_group" "default" {
+  name       = "main"
+  subnet_ids = [aws_subnet.database-subnet-1.id, aws_subnet.database-subnet-1.id]
+tags = {
+    Name = "My DB subnet group"
+  }
+}
+resource "aws_db_instance" "default" {
+  allocated_storage      = 10
+  db_subnet_group_name   = aws_db_subnet_group.default.id
+  engine                 = "mysql"
+  engine_version         = "8.0.20"
+  instance_class         = "db.t2.micro"
+  multi_az               = true
+  name                   = "mydb"
+  username               = "username"
+  password               = "password"
+  skip_final_snapshot    = true
+  vpc_security_group_ids = [aws_security_group.database-sg.id]
+}
+
+# Getting the DNS of load balancer
+output "lb_dns_name" {
+  description = "The DNS name of the load balancer"
+  value       = "${aws_lb.external-alb.dns_name}"
+}
+
+# creates a CloudFront distribution with an S3 origin
+resource "aws_s3_bucket" "b" {
+  bucket = "mybucket"
+
+  tags = {
+    Name = "My bucket"
+  }
+}
+
+resource "aws_s3_bucket_acl" "b_acl" {
+  bucket = aws_s3_bucket.b.id
+  acl    = "private"
+}
+
+locals {
+  s3_origin_id = "myS3Origin"
+}
+
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  origin {
+    domain_name = aws_s3_bucket.b.bucket_regional_domain_name
+    origin_id   = local.s3_origin_id
+
+    s3_origin_config {
+      origin_access_identity = "origin-access-identity/cloudfront/ABCDEFG1234567"
+    }
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = "Some comment"
+  default_root_object = "index.html"
+
+  logging_config {
+    include_cookies = false
+    bucket          = "mylogs.s3.amazonaws.com"
+    prefix          = "myprefix"
+  }
+
+  aliases = ["mysite.example.com", "yoursite.example.com"]
+
+  default_cache_behavior {
+    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = local.s3_origin_id
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "allow-all"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  # Cache behavior with precedence 0
+  ordered_cache_behavior {
+    path_pattern     = "/content/immutable/*"
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD", "OPTIONS"]
+    target_origin_id = local.s3_origin_id
+
+    forwarded_values {
+      query_string = false
+      headers      = ["Origin"]
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    min_ttl                = 0
+    default_ttl            = 86400
+    max_ttl                = 31536000
+    compress               = true
+    viewer_protocol_policy = "redirect-to-https"
+  }
+
+  # Cache behavior with precedence 1
+  ordered_cache_behavior {
+    path_pattern     = "/content/*"
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = local.s3_origin_id
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+    compress               = true
+    viewer_protocol_policy = "redirect-to-https"
+  }
+
+  price_class = "PriceClass_200"
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "whitelist"
+      locations        = ["US", "CA", "GB", "DE"]
+    }
+  }
+
+  tags = {
+    Environment = "production"
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
